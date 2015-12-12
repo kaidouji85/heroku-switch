66f26651177825428eaad8cdbf767dfb5f6751db
kaidouji85 <kaidouji85@gmail.com>
2015-12-12 16:17:48 +0900

    herokuアプリを再起動するボタンを実装した

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,32 @@
-function restartHeroku() {
-    console.log('restart heroku!!');
+/**
+ * herokuアプリを再起動する
+ *
+ * @param{String} apiKey APIキー
+ * @param{String} appName heorkuアプリ名
+ */
+function restartHeroku(apiKey, appName) {
+    $.ajax('https://api.heroku.com/apps/'+appName+'/dynos',{
+        headers:{
+            Accept: 'application/vnd.heroku+json; version=3',
+            Authorization: 'Bearer '+apiKey
+        },
+        global:false,
+        method: 'DELETE'
+    }).then(function(){
+        console.log('success');
+    }).fail(function(){
+        console.log('error');
+    });
+}
+
+/**
+ * RESTARTボタンを押下した際のイベント
+ *
+ */
+function handleButtonClick() {
+    var apiKey = $('.api-key').val();
+    var appName = $('.app-name').val();
+    restartHeroku(apiKey, appName);
 }
 
 var app = {
@@ -16,8 +43,7 @@ var app = {
     },
 
     receivedEvent: function(id) {
-        var restartButton = document.querySelector('.restart-button');
-        restartButton.addEventListener('click',restartHeroku);
+        $('.restart-button').on('click',handleButtonClick);
     }
 };
 
